3cd6b0e11fd4f846d380d86907672f37182f6bf1
Gabriel Santos <gabriel.santos@services.cmsoftware.com.br>
2024-03-08 18:21:45 -0300

    att

--- view/formCad-view.php
+++ view/formCad-view.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Listagem de Clientes</title>
 </head>
 <body>
@@ -14,7 +15,7 @@
             <?php endforeach; ?>
         </ul>
     <?php else : ?>
-        <p>Nenhum cliente encontrado testestesss.</p>
+        <p>Nenhum cliente encontrado.</p>
     <?php endif; ?>
 </body>
 </html>
